377ff451a6cb7e41788c218fbd3bafa0b6a81962
Noah King <noah@horizon3.ai>
2021-02-10 15:58:23 -0500

    test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AboutMe from './pages/aboutme';
+import Portfolio from './pages/portfolio';
+import Contact from './pages/contact';
+import Navbar from './components/navbar';
+import Header from './components/header';
+
+function App() {
+  console.log('test');
+  return (
+    <Router>
+      <Navbar />
+      <Header />
+      <Route exact path="/" component={AboutMe} />
+      <Route exact path="/aboutme" component={AboutMe} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/contact" component={Contact} />
+    </Router>
+  );
+}
+
+export default App;
